c9b616234d844f092e0f9729e6eda6a860a081ea
wsielyfeng <learnroad@163.com>
2021-05-17 23:40:48 +0800

    完成chart-6

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,6 +5,8 @@ import { Chart1 } from '../components/chart-1'
 import { Chart2 } from '../components/chart-2';
 import { Chart3 } from '../components/chart-3';
 import { Chart4 } from '../components/chart-4';
+import { Chart5 } from '../components/chart-5';
+import { Chart6 } from '../components/chart-6';
 
 
 
@@ -22,8 +24,12 @@ export const Home = () => {
           <Chart3></Chart3>
           <Chart4></Chart4>
         </section>
-        <section className="bordered section3"></section>
-        <section className="bordered section4"></section>
+        <section className="bordered section3">
+          <Chart5></Chart5>
+        </section>
+        <section className="section4">
+          <Chart6></Chart6>
+        </section>
         <section className="bordered section5"></section>
       </main>
       <footer>
